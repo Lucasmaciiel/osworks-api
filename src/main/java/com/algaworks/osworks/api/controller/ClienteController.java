23770876836c487f18e8f6c34da21c71844ac86b
Lucas Maciel <lucasmacielgois@gmail.com>
2020-06-17 20:50:50 -0300

    Removendo clientes fixos, utilizando o mysql e entity manager

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -3,6 +3,9 @@ package com.algaworks.osworks.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,21 +13,12 @@ import com.algaworks.osworks.domain.model.Cliente;
 
 @RestController
 public class ClienteController {
-
+	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Lucas");
-		cliente1.setEmail("lucamaciel@gmail.com");
-		cliente1.setTelefone("99700-000");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Michele");
-		cliente2.setEmail("michele@gmail.com");
-		cliente2.setTelefone("9970-0040");
-
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 }
