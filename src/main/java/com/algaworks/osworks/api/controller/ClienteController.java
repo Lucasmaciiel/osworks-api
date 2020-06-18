7ec47a5f1a1708d4968b06594696aa9605ac3e70
Lucas Maciel <lucasmacielgois@gmail.com>
2020-06-17 21:26:31 -0300

    Retornando erro 404 para cliente que não existe

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -2,12 +2,15 @@ package com.algaworks.osworks.api.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
@@ -24,5 +27,17 @@ public class ClienteController {
 			return clienteRepository.findAll();
 //			return clienteRepository.findByNome("Lucas Maciel");
 //			return clienteRepository.findByNomeContaining("Michele");
+	}
+	
+	@GetMapping("/clientes/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId){
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+		
+		//Se conter cliente retorna cod 200
+		if (cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		// Caso contrário retorna 400
+		return ResponseEntity.notFound().build();
 	}
 }
