00ad6ecc66959aee85152fe7217a74d87d24ea56
Lucas Maciel <lucasmacielgois@gmail.com>
2020-06-17 21:38:30 -0300

    Criando o método adicionar, com status 201 created

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,35 +1,36 @@
 package com.algaworks.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
 import com.algaworks.osworks.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 	
 	@Autowired
 	ClienteRepository clienteRepository;
 	
-	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 			return clienteRepository.findAll();
 //			return clienteRepository.findByNome("Lucas Maciel");
 //			return clienteRepository.findByNomeContaining("Michele");
 	}
 	
-	@GetMapping("/clientes/{clienteId}")
+	@GetMapping("/{clienteId}")
 	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId){
 		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
 		
@@ -40,4 +41,24 @@ public class ClienteController {
 		// Caso contrário retorna 400
 		return ResponseEntity.notFound().build();
 	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
