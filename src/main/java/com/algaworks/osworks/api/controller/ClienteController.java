ec97756c0d55b68be85dab55ed16227f393ec34b
Lucas Maciel <lucasmacielgois@gmail.com>
2020-06-18 16:17:19 -0300

    Implementando Put, Atualizar

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -24,10 +25,9 @@ public class ClienteController {
 	@Autowired
 	ClienteRepository clienteRepository;
 	
+	@GetMapping
 	public List<Cliente> listar() {
 			return clienteRepository.findAll();
-//			return clienteRepository.findByNome("Lucas Maciel");
-//			return clienteRepository.findByNomeContaining("Michele");
 	}
 	
 	@GetMapping("/{clienteId}")
@@ -46,6 +46,17 @@ public class ClienteController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente adicionar(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,@RequestBody Cliente cliente){
+		
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}	
+		cliente.setId(clienteId);
+		cliente = clienteRepository.save(cliente);
+		return ResponseEntity.ok(cliente);
 	}
 }
 
